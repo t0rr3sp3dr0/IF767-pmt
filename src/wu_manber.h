ebee1d0de68aad183d48e7a4886ded912bc03ff3
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2018-10-19 18:24:56 -0300

    Wu-Manber SSA

---
+++ src/wu_manber.h
@@ -0,0 +1,22 @@
+//
+// Created by Pedro Tôrres on 19/10/2018.
+//
+
+#ifndef PMT_WU_MANBER_H
+#define PMT_WU_MANBER_H
+
+
+#include "shift_or.h"
+
+class wu_manber : public shift_or {
+protected:
+    size_t e;
+
+public:
+    wu_manber(std::string &pattern, size_t edit_distance);
+    std::vector<size_t> find(std::string &text) override;
+    bool exists(std::string &text) override;
+};
+
+
+#endif //PMT_WU_MANBER_H
